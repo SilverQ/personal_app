7d0c952ee6b9d1c981a771223aef5f6d2a5e14a9
SilverQ <scor9947@hanmail.net>
2024-09-17 03:15:16 +0900

    1'st commit

--- auto_run.py
+++ auto_run.py
@@ -14,14 +14,23 @@ def update_duckdns():
 
 # GitHub 저장소 업데이트
 def update_github_repo():
-    repo_path = "https://github.com/SilverQ/personal_app"
+    repo_path = "D:\DL_work\personal_app"
     repo = git.Repo(repo_path)
+    # # 작업 디렉토리 경로
+    # print(repo.working_dir)
+    # # Git 디렉토리 경로
+    # print(repo.git_dir)
+    # # 현재 활성 브랜치
+    # print(repo.active_branch)
+    # # 모든 브랜치 목록
+    # for branch in repo.branches:
+    #     print(branch)
     repo.remotes.origin.pull()
 
 
 # Streamlit 서버 실행
 def run_streamlit():
-    streamlit_script = "D:\DL_work\personal_app"
+    streamlit_script = "D:/DL_work/personal_app/"
     subprocess.Popen(["streamlit", "run", streamlit_script])
 
 
